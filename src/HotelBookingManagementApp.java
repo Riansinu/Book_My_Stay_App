98613101c0a0855a37f3dd38268f89470782f025
sheikrizwan2006 <sheikrizwan343@gmail.com>
2026-03-24 10:14:31 +0530

    UC3: Centralized room inventory using HashMap

--- src/HotelBookingManagementApp.java
+++ src/HotelBookingManagementApp.java
@@ -103,6 +103,111 @@ public class HotelBookingManagementApp {
                 System.out.println("Available: " + suiteAvailable + "\n");
 
                 System.out.println("System ready...");
+
+                import java.util.*;
+
+/**
+ * Hotel Booking Management System
+ * Version: 3.1
+ *
+ * Demonstrates centralized room inventory using HashMap.
+ */
+
+// ---------------- ROOM DOMAIN ----------------
+                abstract class Room {
+                    String type;
+                    int beds;
+                    int size;
+                    double price;
+
+                    Room(String type, int beds, int size, double price) {
+                        this.type = type;
+                        this.beds = beds;
+                        this.size = size;
+                        this.price = price;
+                    }
+
+                    void displayDetails() {
+                        System.out.println(type + ":");
+                        System.out.println("Beds: " + beds);
+                        System.out.println("Size: " + size + " sqft");
+                        System.out.println("Price per night: " + price);
+                    }
+                }
+
+                class SingleRoom extends Room {
+                    SingleRoom() {
+                        super("Single Room", 1, 250, 1500.0);
+                    }
+                }
+
+                class DoubleRoom extends Room {
+                    DoubleRoom() {
+                        super("Double Room", 2, 400, 2500.0);
+                    }
+                }
+
+                class SuiteRoom extends Room {
+                    SuiteRoom() {
+                        super("Suite Room", 3, 750, 5000.0);
+                    }
+                }
+
+// ---------------- INVENTORY ----------------
+                class RoomInventory {
+
+                    private HashMap<String, Integer> inventory;
+
+                    RoomInventory() {
+                        inventory = new HashMap<>();
+
+                        // Initialize availability
+                        inventory.put("Single Room", 5);
+                        inventory.put("Double Room", 3);
+                        inventory.put("Suite Room", 2);
+                    }
+
+                    // Get availability
+                    int getAvailability(String roomType) {
+                        return inventory.getOrDefault(roomType, 0);
+                    }
+
+                    // Update availability
+                    void updateAvailability(String roomType, int change) {
+                        int current = inventory.getOrDefault(roomType, 0);
+                        inventory.put(roomType, current + change);
+                    }
+
+                    // Display full inventory
+                    void displayInventory(List<Room> rooms) {
+                        System.out.println("Hotel Room Inventory Status\n");
+
+                        for (Room room : rooms) {
+                            room.displayDetails();
+                            System.out.println("Available Rooms: " + getAvailability(room.type));
+                            System.out.println();
+                        }
+                    }
+                }
+
+// ---------------- MAIN ----------------
+                public class HotelBookingManagementApp {
+
+                    public static void main(String[] args) {
+
+                        // Create room objects
+                        List<Room> rooms = new ArrayList<>();
+                        rooms.add(new SingleRoom());
+                        rooms.add(new DoubleRoom());
+                        rooms.add(new SuiteRoom());
+
+                        // Initialize inventory
+                        RoomInventory inventory = new RoomInventory();
+
+                        // Display inventory
+                        inventory.displayInventory(rooms);
+                    }
+                }
             }
         }
     }
